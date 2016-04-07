46b6a95aff00320d88d8502fae3cfe4faf31f4bb
Anna Weisbrodt <aweisbro@gmail.com>
2016-04-06 18:10:39 -0600

    refactored insert, added methods

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -1,19 +1,22 @@
-require "./lib/node_class.rb"
+require "pry"
+require "./node_class"
 
 
 class CompleteMe
-  attr_accessor :root
+  attr_accessor :parent
   attr_accessor :count
+  attr_accessor :end_node
+  attr_accessor :children
+  attr_accessor :letter
 
   def initialize
-    @root = Node.new
-    @count = 0
+    @parent = {}
   end
 
-
-  def populate(dictionary)
+  def populate(dictionary, count=0)
     word_array = []
     arr = dictionary.split
+
     arr.each do |word|
       word_array << word.downcase
     end
@@ -22,92 +25,88 @@ class CompleteMe
       insert(word)
       count+=1
     end
-
-   word_array
+    count
   end
 
 
-  def insert(word, i=0, node=root)
-    if node.children.empty?
-      Node.new.create_node(word,i,node)
-    elsif node.children.has_key?(word[i])
-      node = node.children(word[i])
-      insert_word(word[i+=1], node)
+  def insert(word, node=nil)
+    node ||= @parent
+    first_letter = word[0]
+    match = node[first_letter]
+      if match
+        insert(word[1..-1], match.children)
+      else
+        node[first_letter] = create_node(Node.new(first_letter), word[1..-1])
+      end
+      inserted = []
+      inserted << word
+      @inserted = inserted
+  end
+
+  def create_node(node, word)
+    if word
+      new_node=Node.new(word[0])
+      node.children[word[0]] = new_node
+      create_node(new_node, word[1..-1])
     else
-      Node.new.create_node(word, i, node)
+    end_node = true
     end
   end
 
+  def suggest(prefix)
+    suggestions = []
+    @inserted.each do |word|
+      if word.include?(prefix)
+        suggestions << word
+      else
+        match = false
+      end
+    end
+  suggestions
+  end
+
+
+  def retrieve_word(word)
+    children = @parent
+    count = 0
+    # while count < children.length
+    #   children = children[word[count]].children
+    #   break if !children
+    #   count += 1
+    # end
+    if children
+      puts "Word exists"
+    else
+      puts "Word does not exist"
+    end
+  end
+
+
+
 end
 
-completion = CompleteMe.new
-completion.count
-dictionary = File.read("/usr/share/dict/words")
-completion.populate(dictionary)
-    # if node.children.empty?
-    #   Node.new.create_node(word,i)
-    #       # create new node for the first letter of the word. This will be the parent node for the word.then insert children for each letter of the word (hash). could be a separate method.
-    #     # end of this method if the above happens
-    #   elsif node.children.has_key?(word[i])
-    #       # if children has includes key of first letter of word, check second letter
-    #     n = word.length
-    #   while i < n
-    #     if !node.children(word[i]).nil?
-    #         # if the key for letter i of word exists in the tree
-    #         i += 1
-    #       else
-    #         break
-    #
-    #   # current_node still = parent node
-    #     # find children (parents hash of children)
-    #     node.children
-    #     new_node.children - look in hash for if key == word[i]
-    #     key - first letter, value - word
-    #       if current letter of word matches any key, but word is not present, insert new value associated with key
-    #   if no matching key, create one for that letter (not a new node)
-
-# #
-# #   def suggest(prefix)
-# #     prefix = prefix.downcase.chars
-# #
-# #     #if this prefix has been entered before,
-# #     # return previously selected (words)
-# #
-# #     # else, proceed with checking trie and returning possible words
-# #
-# #     # make arra for each word, then combine
-# #
-# #     # select(prefix, selected_word)
-# #       # after user makes a selecrion, pass selection to select method
-# #   end
-# #
 # #   def select(prefix, selected_word)
 # #     take prefix (in already-split form)
 # #     search tree - find last letter
 # #     record count (#of times searched) in the appropriate node.
 # #         # found node - count+=1
-# #     end
-# #
-# #   end
+
 # #   # select method - takes a substring and the selected suggestion. You will need to record this selection in your trie and use it to influence future selections to make.
 # #
 #
 #
-#
-#
-# # completion = CompleteMe.new
-# # completion.insert("pizza")
 
-# # completion.count
-# # => 1
-# #
-# # completion.suggest("piz")
-# # => ["pizza"]
-# #
-# # dictionary = File.read("/usr/share/dict/words")
-# #
-# # completion.populate(dictionary)
-# #
+completion = CompleteMe.new
+  # dictionary = File.read("/usr/share/dict/words")
+  # completion.populate(dictionary)
+completion.insert("pizza")
+completion.retrieve_word("pizza")
+
+# completion2 = CompleteMe.new
+# completion2.insert("benz")
+#
+ # completion.suggest("piz")
+# # #
 # # completion.count
 # # => 235886
 # #
