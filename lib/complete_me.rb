41dc36ed214e1ed0bd7eecc042fae156a4dec5c2
Anna Weisbrodt <aweisbro@gmail.com>
2016-04-10 08:51:36 -0600

    insert method refactor, added search method, suggest in progress

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -2,10 +2,7 @@ require "pry"
 require "./lib/node"
 
 class CompleteMe
-  attr_accessor :root
-  attr_accessor :count
-  attr_accessor :end_node
-  attr_accessor :next
+  attr_accessor :root, :count, :next, :end_node
 
   def initialize
     @root = {}
@@ -13,38 +10,76 @@ class CompleteMe
   end
 
   def populate(dictionary, count=0)
-    word_array = []
-    arr = dictionary.split
-    arr.each do |word|
-      word_array << word.downcase
-    end
-    word_array.each do |word|
-      insert(word, i=0, node=root)
-    end
-  end
+    dictionary.split("\n").map(&:downcase).each { |word| insert(word, node=root) }
+   end
 
-  def insert(word, i=0, node=root)
-    x = word.length-1
-    if i == x && node.end_node = true
-      return
-    elsif node.include?(word[i])
-      insert(word, i+=1, node=node.next)
-    else
-      create_node(node, i, word)
-    end
-  end
+  def insert(word,node=root)
+    key = word[0]
+    word = word[1..-1]
 
-  def create_node(node, i, word)
-    x = word.length
-    node = Node.new()
-    node.next[word[i]] = node
-    if i == x
-      node.end_node = true
+    if key == word
+      end_node = true
       @count+=1
+
     else
-    create_node(node, i+=1, word)
+      if !@next.nil? && @next.has_key?(key)
+        @next[key] = node
+        insert(word,node)
+      else
+        node = Node.new
+        @next[key]=node
+        insert(word,node)
+      end
     end
-    node
+    @count
   end
+
+
+  def search(string,node=root, match ="")
+    key = string[0]
+    string = string[1..-1]
+    if string.nil?
+      return match
+    else
+      !@next.nil? && @next.has_key?(key)
+      @next[key] = node
+      search(string,node,match+=key)
+    end
+   end
+
+
+   def suggest(string,node=root, match ="")
+    key = string[0]
+    string = string[1..-1]
+
+    if string.length > 0
+      puts "hitting step 1"
+      !@next.nil? && @next.has_key?(key)
+      @next[key] = node
+      suggest(string,node,match+=key)
+    else
+      puts "on to step 2"
+      @next[key] = node
+      find(node,match+=key)
+    end
+  end
+
+
+  def find(node,match)
+    if end_node == true
+    puts  "end_node is true"
+      return
+    elsif node.nil?
+    puts  "just returning nil"
+      return match
+    else
+    puts  "hitting step 3"
+    # node.keys.map { |key| find(node, match+key)}
+    suggestions = node.keys.map {|key| match+key}
+    end
+  end
+
+      # @next.keys.each do |key|
+      # search(@next[key],match+=key)
 
 end
